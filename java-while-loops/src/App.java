7d99f6d594c2caf34bc5a38ef8a88e4d9ea1c572
github username <um_2003_8@hotmail.com>
2022-06-17 18:44:19 -0400

    create a while loop that prints a number and increase it by 2 if it meets the condition

--- java-while-loops/src/App.java
+++ java-while-loops/src/App.java
@@ -2,8 +2,14 @@
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int number = 20;
+		
+		while(number < 30) {
+			System.out.println("Number is:"+" "+ number);
+			
+			//last line is to prevent from infinity loop 			
+			number += 2; 
+		}
 	}
 
 }
